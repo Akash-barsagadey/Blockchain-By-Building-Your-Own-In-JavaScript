03a0f76966c4f8d38b61b60634f1a23c05b736c6
Akash-barsagadey <akash.bnb2011@gmail.com>
2022-07-15 21:39:38 +0530

    create consensus alogoritham to check valid blockchain

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -17,7 +17,7 @@ Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) 
     const newBlock = {
         index: this.chain.length + 1,
         timeStamp: Date.now(),
-        transaction: this.pendingTransactions,
+        transactions: this.pendingTransactions,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash,
@@ -64,6 +64,32 @@ Blockchain.prototype.proofOfWork = function (previousBlockHash, currentBlockData
     }
 
     return nonce;
+};
+
+Blockchain.prototype.chainIsValid = function (blockchain) {
+    let validChain = true;
+
+    for (var i = 1; i < blockchain.length; i++) {
+        const currentBlock = blockchain[i];
+        const prevBlock = blockchain[i - 1];
+        const blockHash = this.hashBlock(
+            prevBlock["hash"],
+            { transactions: currentBlock["transactions"], index: currentBlock["index"] },
+            currentBlock["nonce"]
+        );
+        if (blockHash.substring(0, 4) !== "0000") validChain = false;
+        if (currentBlock["previousBlockHash"] !== prevBlock["hash"]) validChain = false; // chain not valid
+    }
+
+    const genesisBlock = blockchain[0];
+    const correctNonce = genesisBlock["nonce"] === 100;
+    const correctPreviousBlockHash = genesisBlock["previousBlockHash"] === "0";
+    const correctHash = genesisBlock["hash"] === "0";
+    const correctTransactions = genesisBlock["transactions"].length === 0;
+
+    if (!correctNonce || !correctPreviousBlockHash || !correctHash || !correctTransactions) validChain = false;
+
+    return validChain;
 };
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -20,4 +20,137 @@ const bitcoin = new Blockchain();
 
 // console.log(bitcoin.hashBlock(previousBlockhash, currentBlockData, 142188));
 
-console.log(bitcoin);
+const bc1 = {
+    chain: [
+        {
+            index: 1,
+            timeStamp: 1657898241234,
+            transactions: [],
+            nonce: 100,
+            hash: "0",
+            previousBlockHash: "0",
+        },
+        {
+            index: 2,
+            timeStamp: 1657898875296,
+            transactions: [],
+            nonce: 18140,
+            hash: "0000b9135b054d1131392c9eb9d03b0111d4b516824a03c35639e12858912100",
+            previousBlockHash: "0",
+        },
+        {
+            index: 3,
+            timeStamp: 1657898929875,
+            transactions: [
+                {
+                    amount: 12.5,
+                    sender: "00",
+                    recipient: "38ee0b20045111ed9d076308c82f33fe",
+                    transactionId: "b2deab00045211ed9d076308c82f33fe",
+                },
+                {
+                    amount: 1,
+                    sender: "nvchgvcsgahscjbaja",
+                    recipient: "casghavhvhgagca",
+                    transactionId: "c485b600045211ed9d076308c82f33fe",
+                },
+                {
+                    amount: 20,
+                    sender: "nvchgvcsgahscjbaja",
+                    recipient: "casghavhvhgagca",
+                    transactionId: "cdc65530045211ed9d076308c82f33fe",
+                },
+                {
+                    amount: 30,
+                    sender: "nvchgvcsgahscjbaja",
+                    recipient: "casghavhvhgagca",
+                    transactionId: "d023f440045211ed9d076308c82f33fe",
+                },
+            ],
+            nonce: 60077,
+            hash: "00007f75fc5273e1950550752ab9a6f20d3b299d3d256c52f6447bc4b46506ee",
+            previousBlockHash: "0000b9135b054d1131392c9eb9d03b0111d4b516824a03c35639e12858912100",
+        },
+        {
+            index: 4,
+            timeStamp: 1657898972845,
+            transactions: [
+                {
+                    amount: 12.5,
+                    sender: "00",
+                    recipient: "38ee0b20045111ed9d076308c82f33fe",
+                    transactionId: "d364a050045211ed9d076308c82f33fe",
+                },
+                {
+                    amount: 40,
+                    sender: "nvchgvcsgahscjbaja",
+                    recipient: "casghavhvhgagca",
+                    transactionId: "e3caedf0045211ed9d076308c82f33fe",
+                },
+                {
+                    amount: 50,
+                    sender: "nvchgvcsgahscjbaja",
+                    recipient: "casghavhvhgagca",
+                    transactionId: "e5f9b4d0045211ed9d076308c82f33fe",
+                },
+                {
+                    amount: 60,
+                    sender: "nvchgvcsgahscjbaja",
+                    recipient: "casghavhvhgagca",
+                    transactionId: "e7fa3fc0045211ed9d076308c82f33fe",
+                },
+                {
+                    amount: 70,
+                    sender: "nvchgvcsgahscjbaja",
+                    recipient: "casghavhvhgagca",
+                    transactionId: "eaae3c30045211ed9d076308c82f33fe",
+                },
+            ],
+            nonce: 31531,
+            hash: "00001d7e4bdcf4a34d6f3250b3953c31bd0c13765fb2dd5b32ec1d58bd760dbb",
+            previousBlockHash: "00007f75fc5273e1950550752ab9a6f20d3b299d3d256c52f6447bc4b46506ee",
+        },
+        {
+            index: 5,
+            timeStamp: 1657898990457,
+            transactions: [
+                {
+                    amount: 12.5,
+                    sender: "00",
+                    recipient: "38ee0b20045111ed9d076308c82f33fe",
+                    transactionId: "ed0153f0045211ed9d076308c82f33fe",
+                },
+            ],
+            nonce: 40584,
+            hash: "0000ccf9ee87594a1b518adf2abdaced4b8a5d61c2acc867d07f2942c4d6c6a9",
+            previousBlockHash: "00001d7e4bdcf4a34d6f3250b3953c31bd0c13765fb2dd5b32ec1d58bd760dbb",
+        },
+        {
+            index: 6,
+            timeStamp: 1657898996034,
+            transactions: [
+                {
+                    amount: 12.5,
+                    sender: "00",
+                    recipient: "38ee0b20045111ed9d076308c82f33fe",
+                    transactionId: "f780b4b0045211ed9d076308c82f33fe",
+                },
+            ],
+            nonce: 90034,
+            hash: "0000546f73e9e7df799dbff56f394a8aac0b88da4d8bba5672dd71cdbb49c34c",
+            previousBlockHash: "0000ccf9ee87594a1b518adf2abdaced4b8a5d61c2acc867d07f2942c4d6c6a9",
+        },
+    ],
+    pendingTransactions: [
+        {
+            amount: 12.5,
+            sender: "00",
+            recipient: "38ee0b20045111ed9d076308c82f33fe",
+            transactionId: "fad38930045211ed9d076308c82f33fe",
+        },
+    ],
+    currentNodeUrl: "http://localhost:3001",
+    networkNodes: [],
+};
+
+console.log("VALID:", bitcoin.chainIsValid(bc1.chain));
